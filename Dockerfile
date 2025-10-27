b4f41af148c039d28dc63810930236a882ea523d
Nida Favianka Nawawi <2210512009@mahasiswa.upnvj.ac.id>
2025-10-27 22:11:48 +0700

    Add files via upload
    Upload Tugas Cloud Computing

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+From php:8.2-apache
+Copy . /var/www/html
+EXPOSE 80
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+	echo "<h1>Hello from PHP App Nida!</h1>";
+	echo "<h2>Selamat pagi</h2>";
+	echo "<p>Deployed via DevOps CI/CD pipeline</p>"; 
+?>
